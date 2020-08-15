b65ba1a9082511d36b14568b70d18a1fef7a476f
kosbaship <mohamed.kosba96@gmail.com>
2020-08-15 19:55:50 +0200

    انشاء - Drop Down Menu

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,6 +6,11 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  // الاول - بنعرف القيمه اللي هتتغير هنا
+  // دي هتبق القيمه اللي هتظهر كاول عنصر تم اختياره
+  String dropdownValue = 'USD';
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +48,37 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+//            بنستخدم الاداه بتاعه المنيو
+            child: DropdownButton<String>(
+//              دي القيمه الافتراضيه هتظهر ف الاول
+//              يعني اللي جواها هظهر كاول عنصر تم اختياره
+              value: dropdownValue,
+              items: [
+//                لازم اول قيمه ف المنيو تتساوا مع القيمه الافتراضيه
+                DropdownMenuItem(
+//                  دي القيمه اللي هتتحط بعد الاختيار
+                  value: 'USD',
+//                  دي القيمه اللي هتتعرض علشان نختار منها
+                  child: Text('USD'),
+                ),
+//                هتحط باقي العناصر عادي
+                DropdownMenuItem(
+                  value: 'POUND',
+                  child: Text('POUND'),
+                ),
+//                دا الت عنصر
+                DropdownMenuItem(
+                  value: 'MOU',
+                  child: Text('MOU'),
+                ),
+              ],
+//              لما يحصل تغير استخدم القيمه اللي اتغيرت حطها في القايمه اللي بنعين منها
+              onChanged: (value) {
+                setState(() {
+                  dropdownValue = value;
+                });
+              },
+            ),
           ),
         ],
       ),
